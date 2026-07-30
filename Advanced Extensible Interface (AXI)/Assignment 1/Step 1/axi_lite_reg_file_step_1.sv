8df46c42028efc355d0b42122667a53f335ecbb9
Saransh Choudhary <scsc2699@gmail.com>
2026-07-30 17:57:01 +0530

    Update axi_lite_reg_file_step_1.sv

--- Advanced Extensible Interface (AXI)/Assignment 1/Step 1/axi_lite_reg_file_step_1.sv
+++ Advanced Extensible Interface (AXI)/Assignment 1/Step 1/axi_lite_reg_file_step_1.sv
@@ -1,20 +1,3 @@
-////////////////////////////////////////////////////////////
-/////////////  REUSABLE MACROS FOR ELEGANCE !  ////////////
-///////////////////////////////////////////////////////////
-`define ASYNC_RESET_EN_FF(q, d, en, clk, rst_n) \
-always_ff @(posedge clk or negedge rst_n) begin \
-    if (!rst_n) q <= '0; \
-    else  if(en)      q <= d; \
-    else              q <= q; \   
-end
-
-`define ASYNC_RESET_FF(q, d, clk, rst_n) \
-always_ff @(posedge clk or negedge rst_n) begin \
-    if (!rst_n) q <= '0; \
-    else        q <= d; \   
-end
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Module name : axi_lite_reg_file
 //
@@ -26,6 +9,8 @@ end
 //
 // Author : Saransh Choudhary
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+`include "common/ff_macros.svh"
+
 module axi_lite_reg_file #(
     parameter integer C_S_AXI_DATA_WIDTH = 32,
     parameter integer C_S_AXI_ADDR_WIDTH = 12
